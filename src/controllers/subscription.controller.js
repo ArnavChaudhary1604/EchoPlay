8e433c8f659099f20ee61d6eabccc87016e3a390
Arnav Chaudhary <chaudhary.arnav1604@gmail.com>
2025-08-21 23:38:00 +0530

    feat: implement subscription toggle functionality and enhance subscriber list retrieval

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -4,21 +4,92 @@ import { Subscription } from "../models/subscription.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { channel, subscribe } from "diagnostics_channel"
 
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+    const userId = req.user?._id
+
+    if(!userId){
+        throw new ApiError(404, "No such user exists")
+    }
+
+    const subscriptionStatus = await Subscription.findOne(
+        {
+            subscriber : userId,
+            channel : channelId
+        }
+    )
+
+    if(!subscriptionStatus){
+        const subscribed = await Subscription.create({
+            subscriber : userId,
+            channel : channelId
+        })
+
+        res.json(
+            new ApiResponse(
+                201,
+                subscribed,
+                "User subscribed successfully"
+            )
+        )
+    }
+    else{
+        const unSubscribed = await Subscription.findOneAndDelete({
+            subscriber : userId,
+            channel : channelId
+        })
+
+        res.json(
+            new ApiResponse(
+                200,
+                unSubscribed,
+                "User unsubscribed successfully"
+            )
+        )
+    }
+
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+            
+    const subscriberList =  await Subscription.find(
+        {
+            channel : channelId
+        }
+    ).populate("subscriber", "username")
+
+    res.json(
+        new ApiResponse(
+            200,
+            subscriberList,
+            "Subscriber List fetched successfully"
+        )
+    )
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+
+    const subscribedList =  await Subscription.find(
+        {
+            subscriber : subscriberId
+        }
+    ).populate("channel", "username")
+
+    res.json(
+        new ApiResponse(
+            200,
+            subscribedList,
+            "Subscriber List fetched successfully"
+        )
+    )
 })
 
 export {
