796130f89ad6d196b42bc3c5ca43d1f5e87e8ebe
Arnav Chaudhary <chaudhary.arnav1604@gmail.com>
2025-08-23 00:35:31 +0530

    feat: implement channel stats retrieval with user validation and aggregation for views, videos, subscribers, and likes

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -5,13 +5,128 @@ import {Like} from "../models/like.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { pipeline } from "stream"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const userId = req.user?._id
+    
+    if(!userId){
+        throw new ApiError(404,"Invalid user")
+    }
+
+    const channelStats = await User.aggregate([
+        {
+            $match : {
+                _id : userId
+            }
+        },
+        {
+           $lookup : {
+            from : "videos",
+            localField : "_id",
+            foreignField : "owner",
+            as : "allVideos",
+            pipeline : [
+                {
+                    $project : {
+                        videoFile : 1,
+                        thumbnail : 1,
+                        title : 1,
+                        description : 1,
+                        duration : 1,
+                        views : 1
+                    }
+                }
+            ]
+            }
+        },
+        {
+            $lookup : {
+                from : "subscriptions",
+                localField : "_id",
+                foreignField : "channel",
+                as : "subscribers"
+            }
+        },
+        {
+            $lookup : {
+                from : "likes",
+                let : { videosIds : "$allVideos._id" },
+                pipeline : [
+                    {
+                        $match : {
+                            $expr : {
+                                $in : ["$video", "$$videosIds"]
+                            }
+                        }
+                    }
+                ],
+                as : "videoLikes"
+            }
+        },
+        {
+            $addFields : {
+                totalViews : { 
+                    $sum : "$allVideos.views" 
+                },
+                totalVideos : { 
+                    $size : "$allVideos" 
+                },
+                totalSubscribers : { 
+                    $size : "$subscribers" 
+                },
+                totalLikes : {
+                    $size : "$videoLikes"
+                }
+            }
+        },
+        {
+            $project: {
+                username : 1,
+                email : 1,
+                fullName : 1,
+                avatar : 1,
+                coverImage : 1,
+                allVideos: 1,
+                totalViews: 1,
+                totalVideos: 1,
+                totalSubscribers: 1,
+                totalLikes: 1 
+            }
+        }
+    ])
+
+    res.json(
+        new ApiResponse(
+            200,
+            channelStats,
+            "Channel stats fetched successfully"
+        )
+    )
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    const userId = req.user?._id
+
+    if(!userId){
+        throw new ApiError(404, "No such user exists")
+    }
+
+    const channelVideos = await Video.find(
+        {
+            owner : userId
+        }
+    )
+
+    res.json(
+        new ApiResponse(
+            200,
+            channelVideos,
+            "All videos uploaded by user fetched successfully"
+        )
+    )
 })
 
 export {
